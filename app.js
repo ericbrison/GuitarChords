9d9a8b883048a01ad24a0d5086dce4c9624fd6f3
Eric Brison <ericbrison@users.noreply.github.com>
2026-07-19 08:04:49 +0000

    Notation mixte des notes : E♭, A♭, B♭ au lieu de D♯, G♯, A♯ ; v35

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const state = {
   maxFret: 22,
 };
 
-const APP_VERSION = 'v34';
+const APP_VERSION = 'v35';
 
 /* --- persistance de toutes les options --- */
 const SETT_KEY = 'guitarchords.settings';
